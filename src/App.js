50cb15a0ab162884116d857f7bb459f024362a27
Serapckrkc <Serapckrkc@gmail.com>
2021-06-06 16:43:03 +0300

    files created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <h1 className="brand"><i clasName="fas fa-moon"></i>CoinMoon</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
